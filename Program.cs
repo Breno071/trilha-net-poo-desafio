c5cdc948a02f229444559cc73c3aed829376387b
Breno Narde Silva <brenonarde@gmail.com>
2023-09-22 18:12:36 -0300

    Implementa celulares e classe program

--- Program.cs
+++ Program.cs
@@ -1,3 +1,13 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Console.WriteLine("Smartphone Nokia");
+Smartphone nokia = new Nokia(numero: "77999999999", modelo: "C01 Plus", imei: "111111111", memoria: 64);
+nokia.Ligar();
+nokia.InstalarAplicativo("WhatsApp");
+
+Console.WriteLine();
+
+Console.WriteLine("SmartPhone Iphone");
+Smartphone iphone = new Iphone(numero: "7788888888888", modelo: "15", imei: "222222222222", memoria: 128);
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("DIO");
